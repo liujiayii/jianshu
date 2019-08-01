ec2004df1139d618f0568b4772b26559052b9ce1
LIUJIAYI <liujiayiii@qq.com>
2019-08-01 22:43:48 +0800

    react-redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from "./common/header";
+import {Provider} from 'react-redux'
+import store from "./store";
 import {GlobalStyle} from "./style";
+import {GlobalStyleIcon} from './statics/iconfont/iconfont'
 
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <GlobalStyle/>
+      <GlobalStyleIcon/>
       <Header/>
-    </div>
+    </Provider>
   );
 }
 
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -39,12 +39,29 @@ export const NavItem = styled.div`
     color: #ea6f5a;
   }
 `
+export const SearchWrapper = styled.div`
+  position: relative;
+  float: left;
+  .iconfont{
+    position:absolute;
+    right: 5px;
+    bottom: 5px;
+    width: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 50%;
+    &.focused{
+      background: #777;
+      color: #fff;
+    }
+  }
+`
 export const NavSearch = styled.input.attrs({
   placeholder: '搜索'
 })`
   width: 160px;
   height: 38px;
-  padding: 0 20px;
+  padding: 0 36px 0 20px;
   margin-top: 9px;
   margin-left: 20px;
   box-sizing: border-box;
@@ -53,8 +70,24 @@ export const NavSearch = styled.input.attrs({
   border-radius: 19px;
   background: #eee;
   font-size: 14px;
+  color: #666;
+  &.slide-enter{
+    transition: all .2s ease-out;
+  }
+  &.slide-enter-active{
+    width: 240px;
+  }
+  &.slide-exit{
+    transition: all .2s ease-out;
+  }
+  &.slide-exit-active{
+    width: 160px;
+  }
   &::placeholder{
     color: #999;
+  }
+  &.focused{
+    width: 240px;
   }
 `
 export const Addition = styled.div`
